e22ce83ce8fe0cac762fe10504497816b400f5a3
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-08-24 01:08:46 +0700

    chore: update meta description content

--- client/src/components/shared/Wrapper.js
+++ client/src/components/shared/Wrapper.js
@@ -22,18 +22,18 @@ const Wrapper = ({ children }) => {
         <title>SiabaseCV</title>
         <meta
           name='description'
-          content='A free and open source resume builder that’s built to make the mundane tasks of creating, updating and sharing your resume as easy as 1, 2, 3.'
+          content='A free and open source resume builder that’s built to make the mundane tasks of creating, updating and sharing your resume as easy as writing alphabet.'
         />
         <link rel='canonical' href='https://siabase-cv.web.app/' />
         <meta property='og:url' content='https://siabase-cv.web.app/' />
         <meta property='og:type' content='website' />
         <meta
           property='og:description'
-          content='A free and open source resume builder that’s built to make the mundane tasks of creating, updating and sharing your resume as easy as 1, 2, 3.'
+          content='A free and open source resume builder that’s built to make the mundane tasks of creating, updating and sharing your resume as easy as writing alphabet.'
         />
         <meta
           property='og:image'
-          content='https://siabase-cv.web.app/images/share.png'
+          content='https://siabase-cv.web.app/images/overview.png'
         />
       </Helmet>
 
